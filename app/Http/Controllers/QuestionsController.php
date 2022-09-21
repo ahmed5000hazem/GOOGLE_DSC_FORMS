fcbee6800438000373ebf97960b3990e4a4d88fc
ahmed hazem <ahmed5000hazem@gmail.com>
2022-09-21 11:32:38 +0200

    stoping the question soft deletion

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -88,38 +88,38 @@ class QuestionsController extends Controller
         $question->update(["visible" => $request->visible, "required" => false]);
         return redirect()->back();
     }
-
-    public function restore_questions($id)
-    {
-        $question = Question::withTrashed()->findOrFail($id);
-        $question->restore();
-        return redirect()->back();
-    }
+    // ======= trash section to be continued
+    // public function restore_questions($id)
+    // {
+    //     $question = Question::withTrashed()->findOrFail($id);
+    //     $question->restore();
+    //     return redirect()->back();
+    // }
     
-    public function trash_all($id)
-    {
-        Question::where("form_id", $id)->delete();
-        return redirect()->back();
-    }
+    // public function trash_all($id)
+    // {
+    //     Question::where("form_id", $id)->delete();
+    //     return redirect()->back();
+    // }
     
-    public function restore_all_questions($id)
-    {
-        $question = Question::withTrashed()->where("form_id", $id);
-        $question->restore();
-        return redirect()->route("design-form", ["id" => $id]);
-    }
+    // public function restore_all_questions($id)
+    // {
+    //     $question = Question::withTrashed()->where("form_id", $id);
+    //     $question->restore();
+    //     return redirect()->route("design-form", ["id" => $id]);
+    // }
+    // public function delete_questions($id)
+    // {
+    //     $question = Question::findOrFail($id);
+    //     $question->delete();
+    //     return redirect()->back();
+    // }
 
     public function hard_delete_questions($id)
     {
         $question = Question::withTrashed()->findOrFail($id);
         $question->options()->delete();
         $question->forceDelete();
-        return redirect()->back();
-    }
-    public function delete_questions($id)
-    {
-        $question = Question::findOrFail($id);
-        $question->delete();
         return redirect()->back();
     }
 }
